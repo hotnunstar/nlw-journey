32fef09f9d8fa2d6a5f5726374e71b6a53ba8a1b
Nuno Araújo <nu-nos@hotmail.com>
2024-07-17 11:01:49 +0100

    Correction destination and date header

--- frontend/src/pages/trip-details/destination-and-date-header.tsx
+++ frontend/src/pages/trip-details/destination-and-date-header.tsx
@@ -1,27 +1,33 @@
-import { MapPin, Calendar, Settings2 } from 'lucide-react'
+import { MapPin, Calendar, Share2 } from 'lucide-react'
 import { Button } from '../../components/button'
 import { useParams } from 'react-router-dom'
 import { api } from '../../lib/axios'
 import { useEffect, useState } from 'react'
 import { format } from 'date-fns'
-
-interface Trip {
-  id: string
-  destination: string
-  starts_at: string
-  ends_at: string
-  is_confirmed: boolean
-}
+import { Trip } from '.'
 
 export function DestinationAndDateHeader() {
   const { tripId } = useParams()
   const [trip, setTrip] = useState<Trip | undefined>()
+  const [isLinkCopied, setIsLinkCopied] = useState(false)
 
   useEffect(() => {
     api.get(`/trips/${tripId}`).then((response) => setTrip(response.data.trip))
   }, [tripId])
 
-  const displayedDate = trip ? format(trip.starts_at, "d ' de ' LLL").concat(' até ').concat(format(trip.ends_at, "d ' de ' LLL")) : null
+  const displayedDate = trip ? format(trip.starts_at, 'LLLL d').concat(' - ').concat(format(trip.ends_at, 'LLLL d, u')) : null
+
+  function openLinkCopiedModal() {
+    const currentLink = window.location.href
+    navigator.clipboard
+      .writeText(currentLink)
+      .then(() => {
+        setIsLinkCopied(true)
+        setTimeout(() => {
+          setIsLinkCopied(false)
+        }, 1000)
+      })
+  }
 
   return (
     <div className='px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between'>
@@ -38,11 +44,19 @@ export function DestinationAndDateHeader() {
 
         <div className='w-px h-6 bg-zinc-800' />
 
-        <Button variant='secondary'>
-          Alterar local/data
-          <Settings2 className='size-5' />
+        <Button onClick={openLinkCopiedModal} variant='secondary'>
+          Share trip details
+          <Share2 className='size-5' />
         </Button>
       </div>
+
+      {isLinkCopied && (
+        <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+          <div className='rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+            <p className='text-sm text-zinc-400'>Link copied to clipboard</p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
